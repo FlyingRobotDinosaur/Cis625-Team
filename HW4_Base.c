3b2d14353dc501bc5130f838c6ca39050f7e63fc
Adam Herting <herting@ksu.edu>
2017-10-05 11:04:37 -0500

    Test

---
+++ HW4_Base.c
@@ -0,0 +1,20 @@
+/*
+Homework 4
+CIS 625
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+//char word_array[][];
+//char line_array[][];
+
+loadFile(){
+  FILE *fd;
+  int err;
+  int nwords = 0, nlines = 0;
+
+  //Read in Keywords
+  fd = fopen( "/homes/")
+}
